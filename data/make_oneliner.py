332f47d6610cfe680eddefba7e171bbdb64bbc91
Kanagu-Requiem <the.big.pot1525@gmail.com>
2021-05-05 23:26:55 +0900

    apply oneliner

--- data/make_oneliner.py
+++ data/make_oneliner.py
@@ -3,9 +3,13 @@ import os
 files = ["get_score.js", "get_score_with_detail.js"]
 
 for file in files:
-    with open(file, 'r') as f:
+    if os.path.exists("../" + file):
+        os.remove("../" + file)
+
+for file in files:
+    with open(file, "r") as f:
         data = f.read()
     data = data.replace("    ", "")
     data = data.replace("\n", "")
-    with open("../" + file, mode='w') as f:
+    with open("../" + file, mode="w") as f:
         f.write(data)
